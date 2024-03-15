83ccfad46af3c8404953e330de9f1a4f230f0d9d
Deep-Thakkar-1910 <dthakkar477@gmail.com>
2024-03-16 00:50:22 +0530

    Added trending collection related routes and handlers

--- src/server.js
+++ src/server.js
@@ -3,16 +3,12 @@ const movieRouter = require("./routes/MovieRoutes");
 const TvShowsRouter = require("./routes/TvShowsRoutes");
 const UserRouter = require("./routes/UserRoutes");
 const cookieParser = require("cookie-parser");
+const trendingRoutes = require("./routes/trendingRoutes");
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// initate app
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
 
 // movie related routes
 app.use("/movies", movieRouter);
@@ -20,11 +16,20 @@ app.use("/movies", movieRouter);
 // Tvhsows related routes
 app.use("/tvshows", TvShowsRouter);
 
+// trending route to get trending movies and tv Shows from tmdb api
+app.use("/trending", trendingRoutes);
+
 // using cookieParser for user routes
 app.use(cookieParser());
 
 // user related routes
 app.use("/user", UserRouter);
+
+// global catch for not Found
+app.all("*", (req, res) => {
+  //  if any request apart from the above provided route comes send a 404 error
+  res.sendStatus(404);
+});
 
 // listen for all the requests
 app.listen(process.env.PORT || PORT, () => {

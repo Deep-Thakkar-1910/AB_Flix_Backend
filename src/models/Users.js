c8311a6bf06dc923fac4c7b05661bcbee7d02f67
Deep-Thakkar-1910 <dthakkar477@gmail.com>
2024-03-16 00:07:49 +0530

    refactor Updated user schema

--- src/models/Users.js
+++ src/models/Users.js
@@ -1,12 +1,12 @@
-import mongoose, { Schema } from "mongoose";
+const mongoose = require("mongoose");
 
 // defining the userSchema
-const userSchema = new Schema(
+const userSchema = new mongoose.Schema(
   {
     name: String,
-    email: { type: String, required: true, unique: true },
+    email: { type: String, required: true },
     password: { type: String, required: true },
-    profileImage: String,
+    profileImage: { type: String, required: false },
     watchlist: [
       { type: mongoose.SchemaTypes.ObjectId, ref: ["Movie", "TvShow"] },
     ],
@@ -16,4 +16,6 @@ const userSchema = new Schema(
 );
 
 // defining the user model for consistent data schema along all users
-export const User = mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;

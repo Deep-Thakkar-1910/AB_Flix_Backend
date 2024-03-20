a6a04f9b2e52f126b26e651dbfe0ff67d02f132c
Deep-Thakkar-1910 <dthakkar477@gmail.com>
2024-03-20 18:41:52 +0530

    Added frontend deployment link to allowed cors origins

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -3,7 +3,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
   "https://flixbackend.onrender.com",
-  "https://ab-flix-frontend.onrender.com/",
+  "https://ab-flix-frontend.onrender.com",
 ];
 
 module.exports = allowedOrigins;
